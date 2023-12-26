7c9c4276c801428f31168c29a75fba63b6399632
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-12-26 21:52:34 +0000

    ✨(pages): landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,113 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { addEmailToWaitingList } from "@/lib/appwrite";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [isEmailSent, setIsEmailSent] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleNotify = async () => {
+    setIsLoading(true);
+    addEmailToWaitingList(email)
+      .then((res) => {
+        if (!res) return;
+
+        if (res.status === "200") {
+          setIsEmailSent(true);
+          setEmail("");
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex relative h-screen flex-col md:flex-row items-center md:justify-between">
+      <div className="absolute right-3 bottom-3 text-gray-400 text-sm">
+        Being built by{" "}
+        <a
+          href="https://twitter.com/HeyItzaMi"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          Rui Sousa
+        </a>
+      </div>
+      <div className="flex flex-col gap-4 items-center justify-center md:flex-1 bg-hero-gradient md:h-full w-full py-10">
+        <div>
+          <h1 className="text-6xl font-bold text-gray-800">
+            Welcome to
+            <br />
+            Animus
+          </h1>
+          <p className="text-xl mt-2">
+            A day-to-day event tracker for
+            <br />
+            anticipatory anxiety
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div className="flex flex-row gap-4 items-center justify-center md:flex-1 p-5 md:p-10">
+        <div className="max-w-lg flex flex-col gap-2">
+          <p className="text-xl">
+            For someone with depression or anxiety, the thought of doing
+            something can be worse than the actual task itself.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="text-xl">
+            Designed for those battling depression or anxiety, Animus is being
+            built as a tool to help you identify triggers and track progress.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {!isEmailSent && (
+            <div className="mt-4 flex flex-col gap-2">
+              <p>
+                If this sounds valuable to you, consider leaving your email
+                below to get notified when we launch!
+              </p>
+              <Input
+                placeholder="Email"
+                type="email"
+                value={email}
+                onChange={(e) => {
+                  if (error) {
+                    setError("");
+                  }
+                  setEmail(e.target.value);
+                }}
+              />
+              {error && <p className="text-red-500">{error}</p>}
+              <Button onClick={handleNotify} disabled={isLoading}>
+                I want to get news from Animus!
+              </Button>
+            </div>
+          )}
+          {isEmailSent && (
+            <div className="text-emerald-600">
+              <p>Thanks for your support!</p>
+              <p>
+                Feel free to follow the developments on{" "}
+                <a
+                  href="https://twitter.com/HeyItzaMi"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline"
+                >
+                  Twitter
+                </a>
+              </p>
+            </div>
+          )}
+        </div>
+        {/* <Button onClick={handleLogin}>Login with Google</Button> */}
       </div>
     </main>
-  )
+  );
 }
